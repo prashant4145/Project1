759ee70efb839c891e8261187436e8b22a892cf2
Prashant Kolase <Prashant Kolase@PrashantKolase>
2021-06-27 19:48:59 +0530

    Tp

---
+++ Prashant/src/main/java/com/prashant/App.java
@@ -0,0 +1,13 @@
+package com.prashant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
